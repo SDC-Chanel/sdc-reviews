41e88f6ea43d324a5251bb3470a90b9e127f556a
Madeline King <madeline.king330@gmail.com>
2023-01-05 01:05:43 -0800

    setup k6 load testing

---
+++ server/__loadtests__/getmeta.js
@@ -0,0 +1,25 @@
+import http from 'k6/http';
+import { check, sleep } from 'k6';
+import { Counter } from 'k6/metrics';
+
+const ErrorCount = new Counter('errors');
+
+export const options = {
+  insecureSkipTLSVerify: true,
+  noConnectionReuse: false,
+  stages: [{ duration: '30s', target: 1000 }],
+  thresholds: {
+    http_req_failed: ['rate<0.01'],
+    http_req_duration: ['p(100)<3000'],
+  },
+};
+
+export default function () {
+  const res = http.get('http://localhost:3000/reviews/meta/40353');
+  const success = check(res, {
+    'status is 200': r => r.status === 200,
+  });
+  if (!success) {
+    ErrorCount.add(1);
+  }
+}
